1322a74b94bcc214c4e6457d4f75dfe97fbbcdc3
Renata <125743156+Renata-lala@users.noreply.github.com>
2024-04-23 15:09:29 +0700

    Add files via upload

---
+++ ООАП лаба 3/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ООАП_лаба_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.forward_butt = new System.Windows.Forms.Button();
+            this.back_butt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.singer_label = new System.Windows.Forms.Label();
+            this.name_label = new System.Windows.Forms.Label();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.start_button = new System.Windows.Forms.Button();
+            this.time_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // forward_butt
+            // 
+            this.forward_butt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.forward_butt.Font = new System.Drawing.Font("Segoe Script", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.forward_butt.Location = new System.Drawing.Point(439, 168);
+            this.forward_butt.Name = "forward_butt";
+            this.forward_butt.Size = new System.Drawing.Size(121, 52);
+            this.forward_butt.TabIndex = 0;
+            this.forward_butt.Text = "Вперёд";
+            this.forward_butt.UseVisualStyleBackColor = false;
+            this.forward_butt.Click += new System.EventHandler(this.forward_butt_Click);
+            // 
+            // back_butt
+            // 
+            this.back_butt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.back_butt.Font = new System.Drawing.Font("Segoe Script", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.back_butt.Location = new System.Drawing.Point(83, 168);
+            this.back_butt.Name = "back_butt";
+            this.back_butt.Size = new System.Drawing.Size(121, 52);
+            this.back_butt.TabIndex = 1;
+            this.back_butt.Text = "Назад";
+            this.back_butt.UseVisualStyleBackColor = false;
+            this.back_butt.Click += new System.EventHandler(this.back_butt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(250, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 54);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Плеер";
+            // 
+            // singer_label
+            // 
+            this.singer_label.AutoSize = true;
+            this.singer_label.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.singer_label.Location = new System.Drawing.Point(76, 110);
+            this.singer_label.Name = "singer_label";
+            this.singer_label.Size = new System.Drawing.Size(207, 40);
+            this.singer_label.TabIndex = 3;
+            this.singer_label.Text = "Исполнитель- ";
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.name_label.Location = new System.Drawing.Point(271, 110);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(131, 40);
+            this.name_label.TabIndex = 4;
+            this.name_label.Text = "название ";
+            // 
+            // stop_button
+            // 
+            this.stop_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.stop_button.Font = new System.Drawing.Font("Segoe Script", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stop_button.Location = new System.Drawing.Point(227, 168);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(95, 52);
+            this.stop_button.TabIndex = 6;
+            this.stop_button.Text = "STOP";
+            this.stop_button.UseVisualStyleBackColor = false;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.start_button.Font = new System.Drawing.Font("Segoe Script", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start_button.Location = new System.Drawing.Point(328, 168);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(85, 52);
+            this.start_button.TabIndex = 7;
+            this.start_button.Text = "START";
+            this.start_button.UseVisualStyleBackColor = false;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // time_label
+            // 
+            this.time_label.AutoSize = true;
+            this.time_label.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.time_label.Location = new System.Drawing.Point(472, 110);
+            this.time_label.Name = "time_label";
+            this.time_label.Size = new System.Drawing.Size(88, 40);
+            this.time_label.TabIndex = 5;
+            this.time_label.Text = "время";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 282);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.time_label);
+            this.Controls.Add(this.name_label);
+            this.Controls.Add(this.singer_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.back_butt);
+            this.Controls.Add(this.forward_butt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button forward_butt;
+        private System.Windows.Forms.Button back_butt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label singer_label;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Label time_label;
+    }
+}
+
---
+++ ООАП лаба 3/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using NAudio.Wave;
+namespace ООАП_лаба_3
+{
+    public partial class Form1 : Form
+    {
+        private TrackEnumerator trackEnumerator;
+        SoundPlayer play = null;
+        string fileName = string.Empty;
+
+        private WaveOutEvent outputDevice; // Объект для воспроизведения аудио с помощью NAudio.
+        private AudioFileReader audioFile; // Объект для чтения аудиофайла из файла и передачи его на воспроизведение.
+
+        private TimeSpan currentPosition; // Переменная для хранения текущей позиции воспроизведения аудио.
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+
+        // Объявление класса TrackEnumerator.
+        public class TrackEnumerator
+        {
+            private readonly List<Track> tracks;
+            // Приватное поле tracks типа List<Track>, которое хранит список треков.
+            private int currentPosition;
+            // Приватное поле currentPosition типа int, которое хранит текущую позицию в списке треков.
+
+            public TrackEnumerator(List<Track> tracks)
+            {
+                this.tracks = tracks;
+                currentPosition = -1;
+                // Устанавливаем начальное значение currentPosition равным -1, что обозначает, что текущая позиция в списке треков не определена.
+            }
+
+            public Track Current
+            {
+                // Свойство Current типа Track.
+                get // Геттер свойства Current.
+                {
+                    if (currentPosition >= 0 && currentPosition < tracks.Count)
+                    {
+                        // Проверяем, находится ли currentPosition в пределах допустимого диапазона индексов треков.
+                        return tracks[currentPosition];
+                        // Если currentPosition находится в пределах допустимого диапазона, возвращаем трек с соответствующим индексом из списка треков.
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (currentPosition < tracks.Count - 1)
+                {
+                    // Проверяем, не находимся ли мы на последней позиции в списке треков.
+                    currentPosition++;
+                    return true;
+                    // Возвращаем true, чтобы указать, что перемещение выполнено успешно.
+                }
+                return false;
+            }
+
+
+            public bool MovePrevious()
+            {
+                if (currentPosition > 0)
+                {
+                    // Уменьшаем currentPosition на 1, чтобы переместиться к предыдущему треку в списке.
+                    currentPosition--;
+                    return true;
+                }
+                return false;
+            }
+        }
+        public class Track
+        {
+            public string singer { get; }
+            public string name { get; }
+            public string time { get; }
+
+            public Track(string singer, string name, string time)
+            {
+                this.singer = singer;
+                this.name = name;
+                this.time = time;
+            }
+
+
+        }
+
+        public class Player
+        {
+            public List<Track> Tracks { get; } = new List<Track>();
+
+            public void AddTrack(string singer, string name, string time)
+            {
+                Tracks.Add(new Track(singer, name, time));
+            }
+        }
+
+        public void Start()
+        {
+            play = new SoundPlayer();
+            Player player = new Player();
+            player.AddTrack("Alla Pugacheva", "Leto", "2:23");
+            player.AddTrack("Alliance", "Na zare", "3:35");
+            player.AddTrack("Forum", "Belaya noch'", "1:56");
+
+            trackEnumerator = new TrackEnumerator(player.Tracks);
+
+            //DisplayCurrentTrack();
+        }
+        private void DisplayCurrentTrack()
+        {
+            // Выводим название текущего трека на экран формы
+            singer_label.Text = trackEnumerator.Current.singer;
+            name_label.Text = trackEnumerator.Current.name;
+            time_label.Text = trackEnumerator.Current.time;
+            fileName = "C:\\Users\\slava\\source\\repos\\mucis\\" + trackEnumerator.Current.name + ".wav";
+            //play.SoundLocation = fileName;
+
+            PlayAudio(fileName);
+            //play.Play();
+
+
+        }
+
+        private void PlayAudio(string fileName)
+        {
+            if (System.IO.File.Exists(fileName))
+            {
+                if (outputDevice != null)
+                {
+                    outputDevice.Dispose(); // Освобождаем ресурсы текущего outputDevice, если он существует.
+                }
+
+                audioFile = new AudioFileReader(fileName); // Создаем новый объект AudioFileReader для чтения аудио из файла.
+                outputDevice = new WaveOutEvent();  // Создаем новый объект WaveOutEvent для воспроизведения аудио.
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+            }
+            else
+            {
+                MessageBox.Show("Аудиофайл не найден.");
+            }
+        }
+
+        private void back_butt_Click(object sender, EventArgs e)
+        {
+            if (trackEnumerator.MovePrevious())
+            {
+                DisplayCurrentTrack();
+            }
+            else
+            {
+                MessageBox.Show("Это первый трек в плейлисте.");
+            }
+
+        }
+
+        private void forward_butt_Click(object sender, EventArgs e)
+        {
+            if (trackEnumerator.MoveNext())
+            {
+                DisplayCurrentTrack();
+            }
+            else
+            {
+                MessageBox.Show("Это последний трек в плейлисте.");
+            }
+
+        }
+
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+
+            if (outputDevice != null)
+            {
+                outputDevice.Stop();
+                currentPosition = audioFile.CurrentTime;
+            }
+
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+
+            if (outputDevice != null && outputDevice.PlaybackState == PlaybackState.Stopped)
+            {
+                audioFile.CurrentTime = currentPosition;
+                outputDevice.Play();
+            }
+        }
+
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace ООАП_лаба_3
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer play = null;
+        string fileName = string.Empty;
+        private Player player;
+        private int currentPosition;
+
+        public Form1()
+        {
+            play = new SoundPlayer();
+            InitializeComponent();
+            Start();
+        }
+
+        public class Track
+        {
+            public string singer { get; }
+            public string name { get; }
+            public string time { get; }
+
+            public Track(string singer, string name, string time)
+            {
+                this.singer = singer;
+                this.name = name;
+                this.time = time;
+            }
+        }
+
+        public class Player
+        {
+            public List<Track> Tracks { get; } = new List<Track>();
+
+            public void AddTrack(string singer, string name, string time)
+            {
+                Tracks.Add(new Track(singer, name, time));
+            }
+        }
+
+        private void Start()
+        {
+            player = new Player();
+            player.AddTrack("artist 1", "song1", "2:23");
+            player.AddTrack("artist 2", "song2", "3:35");
+            player.AddTrack("artist 3", "song3", "1:56");
+
+            currentPosition = 0;
+
+            //DisplayCurrentTrack();
+        }
+
+        private void DisplayCurrentTrack()
+        {
+            if (currentPosition >= 0 && currentPosition < player.Tracks.Count)
+            {
+                singer_label.Text = player.Tracks[currentPosition].singer;
+                name_label.Text = player.Tracks[currentPosition].name;
+                time_label.Text = player.Tracks[currentPosition].time;
+                fileName = "C:\\Users\\slava\\source\\repos\\mucis\\" + player.Tracks[currentPosition].name + ".wav";
+                play.SoundLocation = fileName;
+                play.Play();
+            }
+        }
+
+        private void back_butt_Click(object sender, EventArgs e)
+        {
+            if (currentPosition > 0)
+            {
+                currentPosition--;
+                DisplayCurrentTrack();
+            }
+            else
+            {
+                MessageBox.Show("Это первый трек в плейлисте.");
+            }
+        }
+
+        private void forward_butt_Click(object sender, EventArgs e)
+        {
+            if (currentPosition < player.Tracks.Count - 1)
+            {
+                currentPosition++;
+                DisplayCurrentTrack();
+            }
+            else
+            {
+                MessageBox.Show("Это последний трек в плейлисте.");
+            }
+        }
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            play.Stop();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            play.Play();
+        }
+    }
+}
+*/
